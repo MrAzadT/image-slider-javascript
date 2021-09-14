fe28c584ee5800d06a6a4c68980f2e21cadcd7cd
azadtamal <mrazadt@gmail.com>
2021-09-14 16:21:13 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const img = [
+  "img-1.jpg",
+  "img-2.jpg",
+  "img-3.jpg",
+  "img-4.jpg",
+  "img-5.jpg",
+  "img-6.jpg",
+];
+
+let count = 0;
+let imgId = document.getElementById("imgId");
+setInterval(() => {
+  if (count >= img.length) count = 0;
+  const imgUrl = img[count];
+  console.log(imgUrl);
+  imgId.setAttribute("src", imgUrl);
+  count++;
+}, 1500);
